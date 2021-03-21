6fb4e4a922b6f9825ca147b7f36e44d20fd96c2c
CreepyS <43405227+CreepyS@users.noreply.github.com>
2021-03-21 13:51:54 +0100

    System unit test #2 and others.
    The 5 tests specified in issue #2 have been performed, plus minor changes have been made to issues #3.
    
    Co-Authored-By: AlbaranezJavier <32954090+AlbaranezJavier@users.noreply.github.com>

---
+++ Unit_test/TestIR.py
@@ -0,0 +1,39 @@
+import unittest
+from Unit_test.ImperfectionRecognizer import *
+import pytest
+
+class TestImperfectionRecognizer(unittest.TestCase):
+
+    def test_imperfectionDetector(self):
+
+        imagen = 'imagen'
+        cr = CracksRecognizer()
+        kr = KnotsRecognizer()
+        ir = ImperfectionRecognizer(cr, kr)
+        result = ir.getsAllImperfections(imagen)
+        self.assertEqual(result,'CrackRecognizer imperfectionDetectorKnotsRecognizer imperfectionDetector')
+
+    def test_crackRecognizer_imperfectionSegmentator(self):
+        imagen = 'imagen'
+        cr = CracksRecognizer()
+        result = cr.imperfectionSegmentator(imagen)
+        self.assertEqual(result, 'This has to fail!!')
+
+    def test_knotRecognizer_imperfectionSegmentator(self):
+        imagen = 'imagen'
+        kr = KnotsRecognizer()
+        result = kr.imperfectionSegmentator(imagen)
+        self.assertEqual(result, 'KnotsRecognizer imperfectionSegmentator')
+
+    def test_crackRecognizer_imperfectionCuantification(self):
+        cr = CracksRecognizer()
+        result = cr.imperfectionCuantification()
+        self.assertEqual(result, 'CrackRecognizer imperfectionCuantification')
+
+    def test_knotRecognizer_imperfectionCuantification(self):
+        kr = KnotsRecognizer()
+        result = kr.imperfectionCuantification()
+        self.assertEqual(result, 'KnotsRecognizer imperfectionCuantification')
+
+if __name__ == '__main__':
+    unittest.main()
