e99f82b4aa890bed52a7a7a9e2ed6a9916de3b45
CreepyS <43405227+AndresTena@users.noreply.github.com>
2021-05-01 20:50:45 +0200

    Introduction section #15 , deployment section #16 and system operation section #17 .
    The introduction of the document has been developed, as well as the image installation process, the application deployment diagrams and its sequence diagram.
    
    Co-Authored-By: AlbaranezJavier <32954090+AlbaranezJavier@users.noreply.github.com>

--- calculate_imperfections.py
+++ calculate_imperfections.py
@@ -17,8 +17,14 @@ def calculate_imperfections(path_image):
     # Get imperfections mask and quantification
     mask, percent = ir.imperfections_and_quantification(img)
 
+    concat_horizontal = cv2.hconcat([cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 255 * mask])
+    #cv2.imshow('Mascara', concat_horizontal)
+
     # Show info
-    print(f"Image: {path_image.split('/')[-1]}, percentage of imperfections: {percent}")
+    #print(f"Image: {path_image.split('/')[-1]}, percentage of imperfections: {percent}")
+    print('Image '+str(path_image.split('/')[-1]) + ', percentage of imperfections:' + str(percent))
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
 
 
 #python calculate_imperfections.py -image=./Samples/Train/1
@@ -29,6 +35,6 @@ path_image = args['image']
 
 # Sino me pasan argumentos tomo una ruta predefinida
 if path_image == None:
-    path_image = './Samples/Train/1'
+    path_image = '/tmp/AIVA_2021_AJ/Samples/Train/1'
 path_image += '.png'
 calculate_imperfections(path_image)
