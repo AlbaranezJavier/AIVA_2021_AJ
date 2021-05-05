685740aa71553c32de0517d446b510a5cdc88bcb
CreepyS <43405227+AndresTena@users.noreply.github.com>
2021-05-05 14:44:32 +0200

    Segmentation examples
    Images of examples of well and poorly performed segmentations have been uploaded. In addition, the deployment diagram has been updated.

--- Java/src/co/ortizol/Main.java
+++ Java/src/co/ortizol/Main.java
@@ -6,29 +6,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        File f = new File("ruta.txt"); // Creamos un objeto file
-        String root = f.getAbsolutePath();
-        String[] parts = root.split("\\" + "\\");
-
-        String command1 = "";
-        for (int i = 0; i < parts.length-2; ++i) {
-            command1 += parts[i] + "/";
-        }
-
         //Entrada por teclado
         Scanner scannerin = new Scanner (System.in);
-        System.out.println ("¿Desea analizar una imagen de Train o de Test?");
+        System.out.println ("Do you want to analyze a Train or Test image?");
         String keyboardin = "";
         keyboardin = scannerin.nextLine();
-        System.out.println ("¿Cual es el índice de la imagen que desea anlizar?");
+        System.out.println ("What is the index of the image you want to analyze?");
         keyboardin += "/" + scannerin.nextLine();
 
-        //Comandos
-        String command2 = "cd " + command1;
-        command1 += "venv/Scripts/activate.bat";
-        String command3 = "python calculate_imperfections.py -image=./Samples/" + keyboardin;
+        String command1 = "python3 /tmp/AIVA_2021_AJ/calculate_imperfections.py -image=/tmp/AIVA_2021_AJ/Samples/" + keyboardin;
 
-        Process p = Runtime.getRuntime().exec(command1 + " && " + command2 + " && " + command3); //"C:/Users/andre/Documents/GitHub/AIVA_2021_AJ/venv/Scripts/activate.bat && cd C:/Users/andre/Documents/GitHub/AIVA_2021_AJ && python calculate_imperfections.py");
+        Process p = Runtime.getRuntime().exec(command1); //"C:/Users/andre/Documents/GitHub/AIVA_2021_AJ/venv/Scripts/activate.bat && cd C:/Users/andre/Documents/GitHub/AIVA_2021_AJ && python calculate_imperfections.py");
         BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
         String ret = in.readLine();
         System.out.println(ret);
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2, glob
 import matplotlib.pyplot as plt
 from tools.ImperfectionRecognizer import ImperfectionRecognizer
+from tools.LabelsManager import label2mask
 
 """
 This script performs a detection of the cracks and knots present in a piece of wood.
@@ -14,18 +15,22 @@ if __name__ == '__main__':
     # Load image
     for path in paths:
         img = cv2.imread(path)
+        path_gt = '.' + path.split('.')[1] + '.reg'
 
         # Get imperfections mask and quantification
         mask, percent = ir.imperfections_and_quantification(img)
 
+        mask_gt = label2mask(path_gt, img.shape)
         # Show info
         print(f"Image: {path.split('/')[-1]}, percentage of imperfections: {percent}")
 
-        fig, axs = plt.subplots(1, 2)
-        axs[0].imshow(mask)
-        axs[0].set_title("Mask")
-        axs[1].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-        axs[1].set_title("Original")
+        fig, axs = plt.subplots(1, 3)
+        axs[0].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        axs[0].set_title("Original")
+        axs[1].imshow(mask_gt)
+        axs[1].set_title("GT")
+        axs[2].imshow(mask)
+        axs[2].set_title("Mask")
         fig.suptitle(f'{path}')
         plt.show()
 
